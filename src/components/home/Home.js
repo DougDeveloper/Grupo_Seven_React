eaec7524953a0180287ee86aaf52c4c464112647
grynhas <patrick.d.s.reis@gmail.com>
2022-10-23 16:56:52 -0300

    styled: add effect rotate

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -19,19 +19,27 @@ function Home({ setProduto }) {
     return (
         <div className="home">
             <div className="categorias">
-                <button id={categoria === 'todos'? "checked" : "" } onClick={() => setCategoria("todos")}><FaBoxes /> Todos</button>
-                <button id={categoria === 'farmacia'? "checked" : "" } onClick={() => setCategoria("farmacia")}><FaClinicMedical /> Farmácia</button>
-                <button id={categoria === 'pet'? "checked" : "" } onClick={() => setCategoria("pet")}><FaCat /> Pet</button>
-                <button id={categoria === 'variedades'? "checked" : "" } onClick={() => setCategoria("variedades")}><FaBoxOpen /> Variedades</button>
+                <button id={categoria === 'todos'? "isSelected" : "" } onClick={() => setCategoria("todos")}><FaBoxes /> Todos</button>
+                <button id={categoria === 'farmacia'? "isSelected" : "" } onClick={() => setCategoria("farmacia")}><FaClinicMedical /> Farmácia</button>
+                <button id={categoria === 'pet'? "isSelected" : "" } onClick={() => setCategoria("pet")}><FaCat /> Pet</button>
+                <button id={categoria === 'variedades'? "isSelected" : "" } onClick={() => setCategoria("variedades")}><FaBoxOpen /> Variedades</button>
             </div>
             <div className="produtos">
                 {produtosHome?.map((produto) => (
                     <div className="produto" key={produto.id}>
-                        <img className="prodImg" alt={produto.description} src={produto.image} />
-                        <p className="prodPrice">{produto.price}</p>
-                        <p className="prodDesc">{produto.description}</p>
-                        <div className="comprar">
-                            <button className="buy" onClick={ () => setProduto(produto.id)}>Comprar</button>
+                        <div className="flip-card-front">
+                            <img className="prodImg" alt={produto.description} src={produto.image} />
+                            <p className="prodPrice">{produto.price}</p>
+                            <p className="prodDesc">{produto.description}</p>
+                            <div className="comprar">
+                                <button className="buy" onClick={ () => setProduto(produto.id)}>Comprar</button>
+                            </div>                            
+                        </div>
+                        <div className="flip-card-back">
+                            <p>{produto.texto}</p>
+                            <div className="comprar">
+                                <button className="buy" onClick={ () => setProduto(produto.id)}>Comprar</button>
+                            </div>
                         </div>
                     </div>
                 ))} 
